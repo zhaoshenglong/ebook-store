8d29578651d4c7c14ace2b9c9801ffd8b3041c5f
zhaoshenglong <G245078728@sjtu.edu.cn>
2019-03-19 11:50:52 -0400

    change some style

--- Vue+Vue-Router/src/components/books/store/Sidebar.vue
+++ Vue+Vue-Router/src/components/books/store/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="book-side">
+  <div class="side-bar">
     <ul>
       <li
-        class="menu gradient-text"
+        class="side-menu gradient-text underline"
         v-for="item in menuList"
         :key="item.id"
         @click="display(item.id)"
@@ -58,61 +58,3 @@ export default {
   }
 };
 </script>
-<style scoped>
-#book-side {
-  height: 100%;
-  min-height: 1000px;
-  width: 25%;
-  float: left;
-  padding-top: 50px;
-  border-top-left-radius: 20px;
-  border-bottom-left-radius: 20px;
-  background: -webkit-linear-gradient(
-    to right,
-    rgba(229, 252, 251, 0.7),
-    rgba(0, 255, 255, 1)
-  );
-  background: linear-gradient(
-    to right,
-    rgba(229, 252, 251, 0.7),
-    rgba(255, 255, 255, 0.7)
-  );
-}
-
-.menu {
-  font-weight: 600;
-  font-size: 40px;
-  text-align: left;
-  margin-top: 15px;
-  margin-bottom: 25px;
-  padding: 10px 20px;
-  position: relative;
-}
-.gradient-text {
-  background: -webkit-linear-gradient(white, #444444);
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -webkit-background-clip: text;
-}
-.menu:hover,
-.gradient-text:hover {
-  cursor: pointer;
-  background: #444444;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -webkit-background-clip: text;
-}
-.menu:before {
-  content: "";
-  border-bottom: 2px solid #35a3c4;
-  width: 5px;
-  transition: all 0.25s;
-  height: 100%;
-  position: absolute;
-  left: -10px;
-  bottom: 5px;
-}
-.menu:hover::before {
-  width: 100%;
-}
-</style>
--- Vue+Vue-Router/src/components/books/usr/config/SettingSide.vue
+++ Vue+Vue-Router/src/components/books/usr/config/SettingSide.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>SettingSide</div>
+  <div class="side-bar">
+    <ul>
+      <li class="side-menu underline gradient-text">Profile</li>
+      <li class="side-menu underline gradient-text">Address</li>
+      <li class="side-menu underline gradient-text">Account</li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {};
 </script>
 <style scoped>
+.setting-menu {
+  padding: 10px 8px;
+  border: 1px solid #bdbdbd;
+  position: relative;
+}
 </style>
