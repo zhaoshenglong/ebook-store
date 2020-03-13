f7b0177d28578c96361c365fcdfc1c670128be87
zhaoshenglong <G245078728@sjtu.edu.cn>
2020-03-14 00:38:16 +0800

    Fix order bugs

---
+++ SpringBoot/src/main/java/org/desktop/store/BookClient.java
@@ -0,0 +1,19 @@
+package org.desktop.store;
+
+import com.ibook.bookstore.Entity.Book;
+import com.ibook.bookstore.Service.RemoteBookService;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.rmi.RemoteException;
+
+public class client {
+    public static void main(String[] args) throws NamingException, RemoteException {
+        Context namingContext = new InitialContext();
+        String url = "rmi://localhost/book_srv";
+        RemoteBookService remoteBookService = (RemoteBookService) namingContext.lookup(url);
+        String id = "ff80818170c5314c0170c53212bb0001";
+        Book book = remoteBookService.findBookById(id);
+        System.out.println(book);
+    }
+}
